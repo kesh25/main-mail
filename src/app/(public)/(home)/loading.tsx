46647ec39b7557b9fb53ca047a01003e0d08ab58
Kinyua <kinyua.dn@outlook.com>
2024-09-02 12:28:35 +0300

    loading setup

--- src/app/(public)/(home)/loading.tsx
+++ src/app/(public)/(home)/loading.tsx
@@ -2,5 +2,5 @@ import { Heading3 } from "@/components/ui/typography";
 
 export default function Loading() {
     // You can add any UI inside Loading, including a Skeleton.
-    return <Heading3>Loading...</Heading3>
+    return <></>
   }
